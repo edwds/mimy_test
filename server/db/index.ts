4adf98528aa9ac8c5ecb009e1093113a2601f7f7
EDWD <catchtable@catchtableui-MacBookPro-4.local>
2026-01-12 10:21:31 +0900

    fix: Add informative error for missing DATABASE_URL

--- server/db/index.ts
+++ server/db/index.ts
@@ -5,5 +5,9 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is missing in environment variables.");
+}
+
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql, { schema });
